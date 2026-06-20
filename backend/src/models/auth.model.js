f52917c2af6d37b49c89b0f1ad65d642e9ffec50
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-21 01:24:52 +0530

    feat: authModel config

---
+++ backend/src/models/auth.model.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userScehma.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hashSync(this.password, 10);
+});
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.comparePasswordSync(password, this.password);
+};
+
+const userModel = model("User", userSchema);
+export default userModel;
